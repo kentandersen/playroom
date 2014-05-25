8baaa54745394ccc95f8e2df3f97b08aa6b6873d
Kent Andersen <kentareandersen@gmail.com>
2014-05-25 13:18:30 +0200

    added index.html rjsconf and removed irritation alert

--- config/rjs.conf.js
+++ config/rjs.conf.js
@@ -1,12 +1,11 @@
-{
-    baseUrl: '../src/js',
-    removeCombined: true,
-    dir: '../build',
-    modules: [
+({
+    baseUrl: "../src/js",
+    mainConfigFile: "../src/js/require.conf.js",
+    separateCSS: true,
+    stubModules: ['less'],
+    exclude: ['less'],
+    include: ['main-something'],
+    out: '../build/main.min.js',
 
-        {
-            name: 'app/main'
-        }
 
-    ]
-}
+})
--- src/js/component/comp.js
+++ src/js/component/comp.js
@@ -1,3 +1,3 @@
 define(function(require) {
-    alert("ue?");
+    console.log("ue?");
 });
